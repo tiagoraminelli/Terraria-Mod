cd87bdb2558cbedf30bf55e65b66bea6a84ee830
tiagoraminelli <tiagoraminelli@gmail.com>
2025-05-01 13:08:45 -0300

    Primer mod

---
+++ Content/Items/Weapons/Cavalier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TestMod.Content.Proyectiles;
+
+namespace TestMod.Content.Items.Weapons
+{
+    public class Cavalier : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 64;
+            Item.height = 64;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.autoReuse = true;
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 30;
+            Item.knockBack = 10;
+            Item.crit = 12;
+
+            Item.value = Item.buyPrice(gold: 4, silver: 40);
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item1;
+
+            Item.shoot = ModContent.ProjectileType<CavalierProyectile>();
+            Item.shootSpeed = 10;
+        }
+
+        public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.Frostburn2, 360);
+        }
+    }
+}
---
+++ Content/Proyectiles/CavalierProyectile.cs
@@ -0,0 +1,39 @@
+﻿
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace TestMod.Content.Proyectiles
+{
+    internal class CavalierProyectile : ModProjectile
+    {
+    
+        public override void SetDefaults() 
+        {
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.aiStyle = 0;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = 10;
+            Projectile.timeLeft = 720;
+            Projectile.light = 0.1f;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 0;
+        }
+        public override void AI()
+        {
+            Projectile.ai[0] += 1f;
+            if (Projectile.ai[0] % 20 == 0) 
+            {
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center,
+                Projectile.velocity,ProjectileID.CursedFlameFriendly,20,7);
+            
+            }
+
+        }
+    }
+}
